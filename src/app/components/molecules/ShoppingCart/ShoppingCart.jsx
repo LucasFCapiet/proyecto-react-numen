c342e9029b0126dee053ce37ad3da4e1e0bc28c1
Damian Ezequiel Hazrun <damianhazrun@gmail.com>
2024-03-21 12:14:54 -0300

    Se soluciona errores en carro

--- src/app/components/molecules/ShoppingCart/ShoppingCart.jsx
+++ src/app/components/molecules/ShoppingCart/ShoppingCart.jsx
@@ -1,3 +1,4 @@
+
 "use client"
 import { TYPES } from "@/app/actions/shoppingActions";
 import { useReducer } from "react";
@@ -6,10 +7,24 @@ import CartItem from "./CartItem";
 import shoppingReducer from "./shoppingReducer";
 import { shoppingInitialState } from "./shoppingReducer";
 
+
 const ShoppingCart = () => {
-  
   const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
   const { products, cart } = state;
+  if (typeof window !== "undefined") {
+  const open_cart = document.getElementById("open_cart");
+  const shopping_window = document.getElementById("shopping_window");
+  const close_cart = document.getElementById("close_cart");
+
+  open_cart.addEventListener("click", () => {
+    shopping_window.classList.add("show");
+  });
+
+  close_cart.addEventListener("click", () => {
+    shopping_window.classList.remove("show");
+  });
+   }
+
   const addToCart = (id) => {
     console.log(id);
     dispatch({ type: TYPES.ADD_TO_CART, payload: id });
@@ -30,42 +45,24 @@ const ShoppingCart = () => {
   return (
     <div id="shopping_window" className="shopping-window">
       <div className="window">
-      <button id="close_cart">X</button>
-      <h2>Carrito de Compras</h2>
-      <h3>Productos</h3>
-      <div className="box grid-responsive">
-        {products.map((product) => (
-          <Product key={product.id} data={product} addToCart={addToCart} />
-        ))}
+        <button id="close_cart">X</button>
+        <h2>Carrito de Compras</h2>
+        <h3>Productos</h3>
+        <div className="box grid-responsive">
+          {products.map((product) => (
+            <Product key={product.id} data={product} addToCart={addToCart} />
+          ))}
+        </div>
+        <h3>Carrito</h3>
+        <div className="box">
+          {cart.map((item, index) => (
+            <CartItem key={index} data={item} deleteFromCart={deleteFromCart} />
+          ))}
+        </div>
+        <button onClick={clearCart}>Limpiar Carrito</button>
       </div>
-      <h3>Carrito</h3>
-      <div className="box">
-        {cart.map((item, index) => (
-          <CartItem key={index} data={item} deleteFromCart={deleteFromCart} />
-        ))}
-      </div>
-      <button onClick={clearCart}>Limpiar Carrito</button>
-
-
-      
-      </div>
-      
     </div>
   );
-  
 };
 
-const open_cart = document.getElementById('open_cart');
-const shopping_window = document.getElementById('shopping_window');
-const close_cart = document.getElementById('close_cart');
-
-open_cart.addEventListener('click', () => {
-  shopping_window.classList.add('show');  
-});
-
-close_cart.addEventListener('click', () => {
-  shopping_window.classList.remove('show');
-});
-
 export default ShoppingCart;
-
