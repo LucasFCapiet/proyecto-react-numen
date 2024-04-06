b30d516f47deab6e4a34b3c739111b83d8be9e27
Lucas Fernandez Capiet <lucas.fercap@gmail.com>
2024-04-06 16:55:45 -0300

    Agregado Tellafriend y Logo en Footer

--- src/app/components/molecules/Footer/ItemsConteiner.jsx
+++ src/app/components/molecules/Footer/ItemsConteiner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Item from "./Item";
 import { menuItems, primeryItems } from "@/app/constants";
-
+import Tellafriend from "./Tellafriend";
 
 
 const ItemsContainer = () => {
@@ -9,10 +9,10 @@ const ItemsContainer = () => {
     return(
 
     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 border-none">
+        <img className="h-20 w-60" src="/logo.png" alt="Logo" />
+        <Item categories={menuItems} />
         <Item categories={primeryItems} />
-        <Item categories={menuItems} />
-        <Item categories={menuItems} />
-        <Item categories={menuItems} />
+        <Tellafriend />
     </div>
 
     )
---
+++ src/app/components/molecules/Footer/Tellafriend.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+class TellAFriendBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleSendClick = this.handleSendClick.bind(this);
+  }
+
+  handleMessageChange(event) {
+    this.setState({ message: event.target.value });
+  }
+
+  handleSendClick() {
+    const { message } = this.state;
+
+    // Copiar el contenido al portapapeles
+    navigator.clipboard.writeText(message).then(() => {
+      // Obtener la URL de WhatsApp con el mensaje codificado
+      const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(message)}`;
+      
+      // Redirigir a WhatsApp
+      window.location.href = whatsappUrl;
+      
+      // Limpiar el contenido del cuadro de texto
+      this.setState({ message: "" });
+    }).catch(error => {
+      console.error('Error al copiar al portapapeles: ', error);
+    });
+  }
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <div>
+        <h2>Tell a Friend</h2>
+        <textarea
+          value={message}
+          onChange={this.handleMessageChange}
+          placeholder="Escribe aquí..."
+          rows={4}
+          style={{ width: "80%", maxWidth: "100%", minHeight: "100px", color: "black" }} // Establecer el tamaño y el color del texto
+        />
+        <br />
+        <button onClick={this.handleSendClick}>Copiar y Enviar por Whatsapp</button>
+      </div>
+    );
+  }
+}
+
+export default TellAFriendBox;
