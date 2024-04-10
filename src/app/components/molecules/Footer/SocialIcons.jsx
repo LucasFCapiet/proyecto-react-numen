23b728c8217f63e7578e0a6a351a42e8c53729e6
Nacho Fernandez Capiet <ifcapiet@Nachos-MacBook-Pro.local>
2024-04-10 11:47:13 -0600

    estilo (footer) cambio en el hover para que se vean los iconos que antes se tapaban en el hover

--- src/app/components/molecules/Footer/SocialIcons.jsx
+++ src/app/components/molecules/Footer/SocialIcons.jsx
@@ -11,22 +11,22 @@ const   SocialIcons = () => {
     return (
       <div className="text-custom-hover border-none">
         <a href="https://www.facebook.com" rel="noopener noreferrer">
-        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-gray-100 hover:bg-custom-hover duration-300">
+        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-custom-gray hover:bg-custom-hover duration-300">
         <FaFacebookSquare />
         </span>
         </a>
         <a href="https://www.instagram.com" rel="noopener noreferrer">
-        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-gray-100 hover:bg-custom-hover duration-300">
+        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-custom-gray hover:bg-custom-hover duration-300">
         <FaInstagramSquare />
         </span>
         </a>
         <a href="https://www.twitter.com" rel="noopener noreferrer">
-        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-gray-100 hover:bg-custom-hover duration-300">
+        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-custom-gray hover:bg-custom-hover duration-300">
         <AiFillTwitterCircle />
         </span>
         </a>
         <a href="https://api.whatsapp.com/" rel="noopener noreferrer">
-        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-gray-100 hover:bg-custom-hover duration-300">
+        <span className="p-2 cursor-pointer inline-flex items-center rounded-full bg-custom-white mx-1.5 text-xl hover:text-custom-gray hover:bg-custom-hover duration-300">
         <FaWhatsappSquare />
         </span>
         </a>
